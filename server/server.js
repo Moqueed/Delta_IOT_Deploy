4b70605edcf55a519ec29c08a3460cb46aee1fbf
Moqueed <moqueed.pearl@gmail.com>
2025-09-09 22:54:03 +0530

    cors-setup

--- server/server.js
+++ server/server.js
@@ -33,7 +33,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET,PUT,PATCH,POST,DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.get("/", (req, res) => {
   res.send("Server is Running! Welcome to Delta IoT API.");
